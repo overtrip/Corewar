01dcd1698635dfcdf193dacb7bd8c64163ef3dec
Jeremy ALONSO <jealonso@e3r12p7.42.fr>
2016-11-02 15:37:33 +0100

    test gitignore

--- ressource_apprentissage/pouet.s
+++
@@ -1,4 +0,0 @@
-.name "zork";sfjkbwkjrhwlr
-.comment "I'M ALIIIIVE";wrtgqeg
-
-l2:		sti r1, %5, %1
--- ressources/champions/examples/zork.s
+++
@@ -1,8 +0,0 @@
-.name "zork"
-.comment "I'M ALIIIIVE"
-
-l2:		sti r1, %:coucou, %1
-		and r1, %0, r1
-
-coucou:	live %1
-zjmp %:live
