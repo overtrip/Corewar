f2ce630d21808e6e5607932fd6a9fafd66bb658a
Jeremy ALONSO <jealonso@e3r11p11.42.fr>
2016-12-15 17:18:38 +0100

    Goodbye binome

--- srcs/find_instruction.c
+++ srcs/find_instruction.c
@@ -6,7 +6,7 @@
 /*   By: jealonso <jealonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 18:00:17 by jealonso          #+#    #+#             */
-/*   Updated: 2016/12/15 15:11:42 by jealonso         ###   ########.fr       */
+/*   Updated: 2016/12/15 15:56:37 by jealonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,22 @@
 static int		search_instruction(char *str, int line)
 {
 	int			i;
+	char		*to_find;
+	char		*begin;
 	extern t_op	g_op_tab[17];
 
-	i = 0;
-	if (!(str && *str))
+	i = -1;
+	if (!str || !*str)
 		return (0);
-//	printf("[%s](%d)-> search_instruction\n", str, line);
-	while (g_op_tab[i].name)
-	{
-		if (!ft_strcmp(str, g_op_tab[i].name))
+	begin = str;
+	to_find = ft_strchr(str, ' ');
+	begin = ft_strndup(str, to_find - begin);
+	while (++i && g_op_tab[i].name)
+		if (!ft_strcmp(begin, g_op_tab[i].name))
 		{
-//		printf("[%s]->[%s]\n", str, g_op_tab[i].name);
-			return (i);
+			printf("ICI\n");
+				return (i);
 		}
-		++i;
-	}
-//	printf("[%s](%d)\n", str, line);
 	send_id("token", line);
 	return (0);
 }
@@ -54,42 +54,22 @@ void		fill_instruction(t_head **head, int i, char *str)
 }
 
 /*
-** TODO	ft_strchr like, find multiple occurence in string
-*/
-
-int				multichr(char *str, char *to_find)
-{
-	char	*current;
-
-	current = str;
-	while (*current)
-	{
-		if (ft_strchr(to_find, *current))
-			return (1);
-		++current;
-	}
-	return (0);
-}
-
-/*
-**	TODO check if line is empty or comment
+**	Check if line is a comment or a label
 */
 
 int				go_out(char *str)
 {
-	char	*current;
+	char	*find;
 
-	if (!str || !*str || (str && *str == '#'))
+	if (*str == '#')
 		return (1);
-	current = str;
-	while (*current)
-	{
-		if (!ft_strchr(" #;\t", *current))
-			return (0);
-		++current;
-	}
-	return (1);
+	if (!(find = ft_strchr(str, ':')))
+		return (0);
+	if (++find && !*find)
+		return (1);
+	return (0);
 }
+
 
 /*
 **	Check if  instructions are valide
@@ -99,28 +79,19 @@ int				find_instruction(char **data, unsigned char *flag,
 													int line, t_head **head)
 {
 	char		*new;
-	char		*cast;
 	int			index;
 	int			i;
 	extern t_op	g_op_tab[17];
 
 	i = 0;
-	cast = (char *)data;
 	(void)head;
 	(void)flag;
-	if (go_out(cast))
-		return (0);
-	while ((new = parse_strsep(&cast, " \t")))
+	new = ft_strtrim((char *)data);
+	if (new && *new && !go_out(new))
 	{
-		if ((index = search_instruction(new, line)))
-			break;
-		free(new);
-	}
-	if (new && *new)
-	{
-		new = ft_strtrim(new);
 		if ((index = search_instruction(new, line)))
 		{
+			printf("+%d+\n", index);
 		/*	create_instruction(head, index);
 			while (i < g_op_tab[index].nb_arg)
 			{
@@ -128,7 +99,7 @@ int				find_instruction(char **data, unsigned char *flag,
 				++i;
 			}*/
 		}
-	free(new);
 	}
+	free(new);
 	return (0);
 }
